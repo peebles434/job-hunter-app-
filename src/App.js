20aa3aa1c1e00831b05b69c38803acf775d67240
Ryan Campbell <Rpcampbell434@aol.com>
2020-06-15 14:28:21 -0400

    Setting up Basic App Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createMuiTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
+import "./App.css";
+import JobHunterApp from "./Components/JobHunterApp";
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <JobHunterApp />
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/JobHunterApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./Header";
+
+const JobHunterApp = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Job Hunter</h1>
+    </div>
+  );
+};
+
+export default JobHunterApp;
