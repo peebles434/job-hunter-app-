7a41d0f4510a8af7e3901999e2d96639f3d00636
Ryan Campbell <Rpcampbell434@aol.com>
2020-06-17 19:02:18 -0400

    Moved JobCard styles and logic to sep dir's, updated comapny url line

--- client/src/Components/JobCard.js
+++ client/src/Components/JobCard.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { observer } from "mobx-react";
-import { makeStyles } from "@material-ui/core/styles";
+import { findDaysAgo, linkify } from "../Logic/jobCardLogic";
+import { useStyles } from "../Styles/jobCardStyles";
 import clsx from "clsx";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
@@ -11,7 +12,6 @@ import Collapse from "@material-ui/core/Collapse";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
@@ -19,72 +19,12 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ReactHtmlParser from "react-html-parser";
 import { Link } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 900,
-    minWidth: "sm",
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginBottom: 10,
-    borderRadius: 20,
-  },
-  media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-}));
-
 export const JobCard = observer((job) => {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
-  };
-
-  //   Takes date received
-  const findDaysAgo = () => {
-    const monthArr = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-
-    const isMonthInt = (element) => {
-      return element == job.job.job.created_at.substring(4, 7);
-    };
-
-    let day = parseInt(job.job.job.created_at.substring(8, 10));
-    let month = monthArr.findIndex(isMonthInt);
-    let year = parseInt(job.job.job.created_at.substring(24, 28));
-
-    let postedDate = new Date(year, month, day).getTime();
-    let today = new Date().getTime();
-    let difference = today - postedDate;
-    let daysAgo = Math.floor(difference / 1000 / 60 / 60 / 24);
-    return daysAgo;
   };
 
   return (
@@ -100,17 +40,21 @@ export const JobCard = observer((job) => {
             <MoreVertIcon />
           </IconButton>
         }
-        title={job.job.job.company}
-        subheader={job.job.job.location}
+        title={job.job.company}
+        subheader={job.job.location}
       />
       <CardMedia
         className={classes.media}
-        image={job.job.job.company_logo}
-        title=""
+        image={job.job.company_logo}
+        title={job.job.company}
       />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          Posted {findDaysAgo()} day(s) ago
+          <p>Posted {findDaysAgo(job)} day(s) ago</p>
+          Find {job.job.company}'s Website{" "}
+          <Link href={`${linkify(job)}`} color="inherit" target="_blank">
+            <b>Here</b>
+          </Link>
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -134,7 +78,7 @@ export const JobCard = observer((job) => {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
           <Typography>Description:</Typography>
-          {ReactHtmlParser(job.job.job.description)}
+          {ReactHtmlParser(job.job.description)}
         </CardContent>
       </Collapse>
     </Card>
---
+++ client/src/Logic/jobCardLogic.js
@@ -0,0 +1,41 @@
+//   Takes date ad was posted, manipulates into date(), compares with current date().
+export const findDaysAgo = (job) => {
+  const monthArr = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const isMonthInt = (element) => {
+    return element == job.job.created_at.substring(4, 7);
+  };
+
+  let day = parseInt(job.job.created_at.substring(8, 10));
+  let month = monthArr.findIndex(isMonthInt);
+  let year = parseInt(job.job.created_at.substring(24, 28));
+
+  let postedDate = new Date(year, month, day).getTime();
+  let today = new Date().getTime();
+  let difference = today - postedDate;
+  let daysAgo = Math.floor(difference / 1000 / 60 / 60 / 24);
+  return daysAgo;
+};
+
+//   Takes company_url string from github jobs and returns only the url
+export const linkify = (job) => {
+  if (job.job.company_url) {
+    var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+    return job.job.company_url.replace(urlRegex, function (url) {
+      return url;
+    });
+  }
+};
